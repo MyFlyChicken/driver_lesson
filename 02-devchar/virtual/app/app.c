9e5db6653b2459de0bfab274ac63876942c180c1
禹云飞 <yuyf@audfly2020.com>
2024-08-20 11:07:10 +0800

    排版虚拟字符设备

--- 02-devchar/virtual/app/app.c
+++ 02-devchar/virtual/app/app.c
@@ -4,30 +4,27 @@
 #include <fcntl.h>
 #include <unistd.h>
 #include <string.h>
-int main(int argc, char const *argv[])
+int main(int argc, char const* argv[])
 {
-    int fd = open("/dev/devchar",O_RDWR); 
-    if(fd == -1)
-    {
+    int fd = open("/dev/devchar", O_RDWR);
+    if (fd == -1) {
         perror("open err");
         return -1;
     }
     char buf[128] = {0};
-    fgets(buf,sizeof(buf),stdin);
-    int nbytes = write(fd,buf,strlen(buf));
-    if(nbytes == -1)
-    {
+    fgets(buf, sizeof(buf), stdin);
+    int nbytes = write(fd, buf, strlen(buf));
+    if (nbytes == -1) {
         perror("write err：\n");
         return -1;
     }
-    memset(buf,0, sizeof(buf));
-    nbytes = read(fd,buf,sizeof(buf)-1);
-    if(nbytes == -1)
-    {
+    memset(buf, 0, sizeof(buf));
+    nbytes = read(fd, buf, sizeof(buf) - 1);
+    if (nbytes == -1) {
         perror("read err:\n");
         return -1;
     }
-    printf("接收的数据为：%s\n",buf);
+    printf("接收的数据为：%s\n", buf);
 
     close(fd);
 
